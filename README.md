20baf601c9d21c00ac84ccedb353764bedb9db8c
Jared Reando <jaredreando@gmail.com>
2018-10-26 15:04:52 -0700

    Add function that successfully checks if 0 to user input #s are divisible by 3. Replaces those numbers in list and returns full list.

--- README.md
+++ README.md
@@ -17,11 +17,11 @@ _A web application that takes a number from a user and returns a range of number
 ### Specs
 | Spec | Input | Output |
 | :-------------     | :------------- | :------------- |
-| **If a number containing a 0 is entered, all digits are replaced with "Beep!"** | Example input: "0" | Example Output: "Beep!"|
-| **If a number containing a 1 is entered, all digits are replaced with "Boop!** | Example input: "1" | Example Output: "Boop!"|
-| **If a number divisible by 3 is entered, all digits are replaced with "I'm sorry, Dave. I'm afraid I can't do that.** | User input: "3" | Output: "I'm sorry, Dave. I'm afraid I can't do that."|
-| **If a number higher than 0 is entered, all numbers in the range from 0 to the inputted number are returned.** | User input: "5" | Output: "Beep!", "Boop!", 2, "I'm Sorry...", 4, 5|
-| **If a number contains 0, 1, and 3, the highest order rule is applied to all returned numbers.** | User input: "108" | Output: "..., I'm sorry, Dave. I'm afraid I can't do that."|
+| **If a number containing a 0 is entered, all digits are replaced with "Beep!"** | Example input: "0" | Example output: "Beep!"|
+| **If a number containing a 1 is entered, all digits are replaced with "Boop!"** | Example input: "1" | Example output: "Beep!", "Boop!"|
+| **If a number divisible by 3 is entered, all digits are replaced with "I'm sorry, Dave. I'm afraid I can't do that.** | Example input: "3" | Example Output: "Beep!", "Boop!", "I'm sorry, Dave. I'm afraid I can't do that."|
+| **If a number higher than 0 is entered, all numbers in the range from 0 to the inputted number are returned.** | Example input: "5" | Example output: "Beep!", "Boop!", 2, "I'm Sorry...", 4, 5|
+| **If a number contains 0, 1, and 3, the highest order rule is applied to all returned numbers.** | Example input: "108" | Example output: "..., I'm sorry, Dave. I'm afraid I can't do that."|
 
 
 
--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,52 @@
-var incrementInput = function (input) {
+// var incrementInput = function (input) {
+//   var incrementArray = [];
+//   for(i=0; i <= input; i++) {
+//     incrementArray.push(i);
+//   }
+//   console.log(incrementArray);
+//   return incrementArray;
+// }
+//
+// var findOne = function (input) {
+//   var functionArray = incrementInput(input);
+//   var indexZero = functionArray.indexOf(0);
+//   var indexOne = functionArray.indexOf(1);
+//   if (input % 3 === 0)
+//   var indexThree = functionArray.indexOf(3);
+//
+//   return "zero @: " + indexZero + "<br>one @: " + indexOne + "<br>three @: " + indexThree;
+//
+//   return functionArray;
+//
+// }
+
+
+
+var beepBoop = function(input) {
+// Takes user input number and creates an array with all integers between '0' and input#:
   var incrementArray = [];
-  for(i=0; i <= input; i++) {
+  for (var i = 0; i <= input; i++) {
     incrementArray.push(i);
   }
-  console.log(incrementArray);
-  return incrementArray;
-}
 
-var findOne = function (input) {
-  var functionArray = incrementInput(input);
-  var indexZero = functionArray.indexOf(0);
-  var indexOne = functionArray.indexOf(1);
-  if (input % 3 === 0)
-  var indexThree = functionArray.indexOf(3);
+  var beepBoopArray = [];
 
-  return "zero @: " + indexZero + "<br>one @: " + indexOne + "<br>three @: " + indexThree;
-
-  return functionArray;
-
-}
-
-var moneyMaker = function(input) {
-  var functionArray = incrementInput(input);
-
-  functionArray.forEach(function(arrayItem) {
-    /*sets variable to result of indexOf of '0'. If zero is present, will return "Beep"    */
-    var beepBoopCheck = toString(arrayItem).indexOf(0);
-    switch (beepBoopCheck) {
-      case "-1":
-      arrayItem = "Beep!";
-      break;
-
-      default:
-      break;
-
+  for (var i = 0; i < incrementArray.length; i++) {
+    // checks if input number is divisible by 3 and is not '0'
+    if (incrementArray[i] % 3 === 0 && incrementArray[i] !== 0) {
+      beepBoopArray.push(" " + "I'm sorry, Dave. I'm afraid I can't do that.");
+      // converts array element to string, then back to array as separate elements, then checks for "1" value
+    } else {
+      beepBoopArray.push(incrementArray[i]);
     }
-  });
+  }
 
-  return functionArray;
-}
+    return beepBoopArray;
+
+};
+
+
+
 
 
 
@@ -48,7 +56,7 @@ $(document).ready(function(){
     event.preventDefault();
 
     var userInput = $("input#userInput").val();
-    var userOutput = moneyMaker(userInput);
+    var userOutput = beepBoop(userInput);
 
     $("#boopOutput").text(userOutput);
 
