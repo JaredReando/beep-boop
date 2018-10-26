3740677ff02ad56582a323d23acddb883b5302be
Jared Reando <jaredreando@gmail.com>
2018-10-26 11:31:13 -0700

    Add increment function to output all numbers up to and including user input number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-var newFunction = fucntion
+var incrementInput = function (input) {
+  var incrementArray = []
+  for(i=0; i <= input; i++) {
+    incrementArray.push(i + " ");
+  }
+  return incrementArray;
+}
+
+
+
+
+
+
+
+// client-side code:
+$(document).ready(function(){
+  $("#boopForm").submit(function(event){
+    event.preventDefault();
+
+    var userInput = $("input#userInput").val();
+    var userOutput = incrementInput(userInput);
+
+    $("#boopOutput").text(userOutput);
+
+
+  });
+});
